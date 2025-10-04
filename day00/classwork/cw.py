3e569e35433c40f44091e9e3be5e2456c5083149
vladesS69 <sabavalu2@gmail.com>
2025-10-05 00:38:03 +0400

    commit

--- day00/classwork/cw.py
+++ day00/classwork/cw.py
@@ -1,1 +1,17 @@
 # no class work 
+print("saba")
+my_name = "saba"
+print(my_name)
+my_number = 5
+print(my_number)
+
+
+item_price = 10.99
+print(item_price)
+city = "Tbilisi"
+print(city)
+age = 16
+print(age)
+name="saba"
+print(name)
+
